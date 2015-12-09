5e3f78d951230f98f6b77b5b172a59b7012394c4
chtmorris <agoosester@gmail.com>
2015-12-09 18:19:08 +0800

    Added revolving words

---
+++ assets/js/rotatingWords.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $( ".build-word" ).hide();
+    $( ".launch-word" ).hide();
+    $( ".design-word" ).delay(2000).fadeOut();
+    $( ".build-word" ).delay(3000).fadeIn();
+    $( ".build-word" ).delay(2000).fadeOut();
+    $( ".launch-word" ).delay(6000).fadeIn();
+    $( ".tagline" ).delay(8000).fadeOut();
+});
