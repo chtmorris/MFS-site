1027aec9cf3035c5f75f46cd0b5e0708b12ef6bf
chtmorris <agoosester@gmail.com>
2016-01-20 17:48:35 +0800

    Update to Flailing Whale site

--- assets/js/rotatingWords.js
+++ assets/js/rotatingWords.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-  $( ".tagline-design, .tagline-build, .tagline-launch" ).css('opacity', '0.01');
-  $( ".tagline-design").delay(2000).fadeTo("slow", 1);
-  $( ".tagline-build").delay(3000).fadeTo("slow", 1);
-  $( ".tagline-launch").delay(4000).fadeTo("slow", 1);
-  $( ".description").delay(7000).fadeOut("slow");
+  // $( ".tagline-design, .tagline-build, .tagline-launch" ).css('opacity', '0.01');
+  // $( ".tagline-design").delay(2000).fadeTo("slow", 1);
+  // $( ".tagline-build").delay(3000).fadeTo("slow", 1);
+  // $( ".tagline-launch").delay(4000).fadeTo("slow", 1);
+  // $( ".description").delay(7000).fadeOut("slow");
 });
